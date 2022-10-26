58b235ebd339e7bebf56d1975795b8035ef1c74e
Creiwry <jay.gardos@outlook.com>
2022-10-26 11:54:27 +0200

    edited migrations to include columns

--- db/migrate/20221026094821_create_dogsitters.rb
+++ db/migrate/20221026094821_create_dogsitters.rb
@@ -1,7 +1,8 @@
 class CreateDogsitters < ActiveRecord::Migration[7.0]
   def change
     create_table :dogsitters do |t|
-
+      t.belongs_to :city, index: true
+      t.string :name
       t.timestamps
     end
   end
--- db/migrate/20221026094832_create_dogs.rb
+++ db/migrate/20221026094832_create_dogs.rb
@@ -1,7 +1,8 @@
 class CreateDogs < ActiveRecord::Migration[7.0]
   def change
     create_table :dogs do |t|
-
+      t.belongs_to :city, index: true
+      t.string :name
       t.timestamps
     end
   end

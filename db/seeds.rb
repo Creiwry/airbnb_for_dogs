bd1ab6d6e81898db30d0067598408e2861fbc25e
Creiwry <jay.gardos@outlook.com>
2022-10-26 12:08:26 +0200

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+
+#Data for Cities
+
+10.times do |city|
+  city = City.create(name: Faker::Address.city)
+end
+
+cities = City.all
+
+
+#Data for Dogsitters
+
+100.times do |dogsitter|
+  dogsitter = Dogsitter.create(city: cities.sample, name: Faker::Name.name)
+end
+
+dogsitters = Dogsitter.all
+
+#Data for Dogs
+
+100.times do |dog|
+  dog = Dog.create(city: cities.sample, name: Faker::Creature::Dog.name)
+end
+
+dogs = Dog.all
+
+#Data for Strolls
+
+200.times do |stroll|
+  stroll = Stroll.create(dog: dogs.sample, dogsitter: dogsitters.sample)
+end
